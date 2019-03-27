3527f65c02b0f3b12156ac5f6eed5cb9d5ed7c7a
subhransupatra <footcri@gmail.com>
2019-03-27 13:01:43 -0700

    today commit

---
+++ ReadDyn.py
@@ -0,0 +1,56 @@
+from __future__ import print_function # Python 2/3 compatibility
+import boto3
+import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError
+from flask import Flask, request
+from flask_restful import Resource, Api
+#from flask.ext.jsonpify import jsonify
+
+
+app = Flask(__name__)
+api = Api(app)
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+class ReadfromDynamo(Resource):
+    def get(self):
+
+        dynamodb = boto3.resource("dynamodb", 
+                            region_name='us-east-2', 
+                            aws_access_key_id= "AKIAJKXKJBCMRDJ65I2A",
+                            aws_secret_access_key= "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",
+                            endpoint_url="http://dynamodb.us-east-2.amazonaws.com")
+
+        table = dynamodb.Table('MovieDetails')
+
+        title = "quantum of solace"
+        Movieid = 1
+
+        try:
+            response = table.get_item(
+            Key={
+                'MovieID': Movieid,
+                'Title': title
+            }
+            )
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+        else:
+            item = response['Item']
+            print("GetItem succeeded:")
+            result = json.dumps(item, indent=4, cls=DecimalEncoder)
+            return result
+
+api.add_resource(ReadfromDynamo, '/getdata')
+if __name__ == '__main__':
+     app.run(port='8080')
